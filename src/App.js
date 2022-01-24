7b5886c8b3205501e549364757acdfb47f28dc2d
pi22by7 <70760826+pi22by7@users.noreply.github.com>
2022-01-24 23:28:35 +0530

    working selectors and angular implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
+"use strict";
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+angular.module("App", ["ngMaterial"]).controller("picCtrl", [
+  "$scope",
+  "Menus",
+  function display($scope, Global) {
+    $scope.global = Global;
+    $scope.image = {
+      food: "favicon.png",
+    };
+  },
+]);
